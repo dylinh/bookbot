307e05f74d674f06b71529be9a6c9452189946f4
Dylinh Bun <dylinhb@gmail.com>
2025-10-26 10:48:59 -0400

    allowed path to be passed in arg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters, sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,15 +7,19 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt")
-    print("----------- Word Count ----------")
-    print(f"Found {count_words(book_text)} total words")
-    print("--------- Character Count -------")
-    sorted_chars = sorted_list(count_characters(book_text))
-    for obj in sorted_chars:
-        if obj["char"].isalpha():
-            print(f"{obj["char"]}: {obj["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) == 2:
+        book_text = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt")
+        print("----------- Word Count ----------")
+        print(f"Found {count_words(book_text)} total words")
+        print("--------- Character Count -------")
+        sorted_chars = sorted_list(count_characters(book_text))
+        for obj in sorted_chars:
+            if obj["char"].isalpha():
+                print(f"{obj["char"]}: {obj["num"]}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
